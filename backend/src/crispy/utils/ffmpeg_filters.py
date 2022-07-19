bb50073f305b49600e3e746ddaf7d6e17a40dd91
Nicolas Charles <nicolascharles9121@gmail.com>
2022-07-19 17:20:20 +0200

    feat: Added logger for wrong type

--- backend/src/crispy/utils/ffmpeg_filters.py
+++ backend/src/crispy/utils/ffmpeg_filters.py
@@ -1,67 +1,111 @@
 from typing import Union
+from utils.constants import L
 import ffmpeg
 
 
 def crop(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.cop(960, 540) if type(option) == bool and option else video
+    if type(option) == bool and option:
+        video = video.crop(960, 540)
+    else:
+        L.error(
+            f"expected type({bool}) got ({type(option)}) for filter ('crop')")
     return video
 
 
 def blur(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.filter("boxblur", option) if type(option) == int else video
+    if type(option) == int or type(option) == float:
+        video = video.filter("boxblur", option)
+    else:
+        L.error(
+            f"expected type({int} / {float}) got ({type(option)}) for filter ('blur')"
+        )
     return video
 
 
 def scale(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.filter("scale", option) if type(option) == str else video
+    if type(option) == str:
+        video = video.filter("scale", option)
+    else:
+        L.error(
+            f"expected type({str}) got ({type(option)}) for filter ('scale')")
     return video
 
 
 def hflip(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.hflip() if type(option) == bool and option else video
+    if type(option) == bool and option:
+        video = video.hflip()
+    else:
+        L.error(
+            f"expected type({bool}) got ({type(option)}) for filter ('hflip')")
     return video
 
 
 def vflip(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.vflip() if type(option) == bool and option else video
+    if type(option) == bool and option:
+        video = video.hflip()
+    else:
+        L.error(
+            f"expected type({bool}) got ({type(option)}) for filter ('vflip')")
     return video
 
 
 def brightness(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.hue(b=option) if type(option) == int else video
+    if type(option) == int or type(option) == float:
+        video = video.hue(b=option)
+    else:
+        L.error(
+            f"expected type({int} / {float}) got ({type(option)}) for filter ('brightness')"
+        )
     return video
 
 
 def saturation(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.hue(s=option) if type(option) == int else video
+    if type(option) == int or type(option) == float:
+        video = video.hue(s=option)
+    else:
+        L.error(
+            f"expected type({int} / {float}) got ({type(option)}) for filter ('saturation')"
+        )
     return video
 
 
 def zoom(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.zoompan(
-        z=option, fps=60, d=1, x="iw/2-(iw/zoom/2)",
-        y="ih/2-(ih/zoom/2)") if type(option) == int else video
+    if type(option) == int or type(option) == float:
+        video = video.zoompan(z=option,
+                              fps=60,
+                              d=1,
+                              x="iw/2-(iw/zoom/2)",
+                              y="ih/2-(ih/zoom/2)")
+    else:
+        L.error(
+            f"expected type({int} / {float}) got ({type(option)}) for filter ('zoom')"
+        )
     return video
 
 
 def grayscale(
-        option: Union[str, bool, int],
+        option: Union[str, bool, int, float],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.hue(s=0) if type(option) == bool and option else video
+    if type(option) == bool and option:
+        video = video.hue(s=0)
+    else:
+        L.error(
+            f"expected type({bool}) got ({type(option)}) for filter ('grayscale')"
+        )
     return video
