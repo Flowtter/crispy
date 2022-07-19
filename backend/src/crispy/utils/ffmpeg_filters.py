589b035fd534380fb64312ef2926cc2fe9a16a4c
Nicolas Charles <nicolascharles9121@gmail.com>
2022-07-19 16:51:17 +0200

    feat: Fixed type check for filters

--- backend/src/crispy/utils/ffmpeg_filters.py
+++ backend/src/crispy/utils/ffmpeg_filters.py
@@ -5,69 +5,63 @@ import ffmpeg
 def crop(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    if option:
-        video = video.crop(x=960, y=540)
+    video = video.cop(960, 540) if type(option) == bool and option else video
     return video
 
 
 def blur(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.filter("boxblur", option)  # "luma_radius=2:luma_power=1"
+    video = video.filter("boxblur", option) if type(option) == int else video
     return video
 
 
 def scale(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.filter("scale", option)  # "w=1280:h=720"
+    video = video.filter("scale", option) if type(option) == str else video
     return video
 
 
 def hflip(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    if option:
-        video = video.hflip()
+    video = video.hflip() if type(option) == bool and option else video
     return video
 
 
 def vflip(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    if option:
-        video = video.vflip()
+    video = video.vflip() if type(option) == bool and option else video
     return video
 
 
 def brightness(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.hue(b=option)
+    video = video.hue(b=option) if type(option) == int else video
     return video
 
 
 def saturation(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.hue(s=option)
+    video = video.hue(s=option) if type(option) == int else video
     return video
 
 
 def zoom(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    video = video.zoompan(z=option,
-                          fps=60,
-                          d=1,
-                          x="iw/2-(iw/zoom/2)",
-                          y="ih/2-(ih/zoom/2)")
+    video = video.zoompan(
+        z=option, fps=60, d=1, x="iw/2-(iw/zoom/2)",
+        y="ih/2-(ih/zoom/2)") if type(option) == int else video
     return video
 
 
 def grayscale(
         option: Union[str, bool, int],
         video: ffmpeg.nodes.FilterableStream) -> ffmpeg.nodes.FilterableStream:
-    if option:
-        video = video.hue(s=0)
+    video = video.hue(s=0) if type(option) == bool and option else video
     return video
