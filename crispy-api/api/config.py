212a17dfab42ce360136d34a47757fdcc7152b30
Flowtter <naexys.naexys@gmail.com>
2023-01-11 19:49:03 +0100

    feat: Setup ffmpeg and the environnement

--- crispy-api/api/config.py
+++ crispy-api/api/config.py
@@ -6,3 +6,7 @@ DEBUG = config("DEBUG", cast=bool, default=True)
 HOST = config("HOST", default="0.0.0.0")
 PORT = config("PORT", cast=int, default=1337)
 MONGO_URI = config("MONGO_URI", default="mongodb://localhost/tests")
+
+ASSETS = config("ASSETS", default="assets")
+
+ASSETS = "assets"
