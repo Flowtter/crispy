baeccf5483d5ed5666db2da71718e4bee4e14d7d
Brice Parent <34689945+Flowtter@users.noreply.github.com>
2023-01-16 21:12:04 +0100

    feat: Migrate-AI (#29)

--- crispy-api/tests/constants.py
+++ crispy-api/tests/constants.py
@@ -5,3 +5,7 @@ MAIN_VIDEO_NO_AUDIO = os.path.join("tests", "assets", "main-video-no-audio.mp4")
 MAIN_VIDEO_OVERWATCH = os.path.join("tests", "assets", "main-video-overwatch.mp4")
 
 ROOT_ASSETS = os.path.join("tests", "assets")
+
+CSV_PATH_XOR = os.path.join(ROOT_ASSETS, "xor.csv")
+CSV_PATH_OVERWATCH = os.path.join(ROOT_ASSETS, "overwatch.csv")
+OVERWATCH_NETWORK = os.path.join("assets", "overwatch.npy")
---
+++ crispy-api/tests/tools/AI/trainer.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+import numpy
+
+from api.tools.AI.trainer import Trainer
+from api.tools.AI.trainer import test as trainer_test
+from api.tools.AI.trainer import train
+from tests.constants import CSV_PATH_XOR
+
+numpy.random.seed(2)
+
+
+def test_trainer(capsys, tmp_path):
+    trainer = Trainer([2, 4, 2], 0.1)
+    trainer.initialize_weights()
+
+    assert not trainer_test(trainer, CSV_PATH_XOR)
+
+    train(5000, trainer, CSV_PATH_XOR, False, tmp_path)
+
+    assert trainer_test(trainer, CSV_PATH_XOR)
+
+    train(30, trainer, CSV_PATH_XOR, True, tmp_path)
+
+    assert len(os.listdir(tmp_path)) == 3
+
+    shutil.rmtree(tmp_path)
+    os.mkdir(tmp_path)
+
+
+def test_print(capsys):
+    trainer = Trainer([2, 4, 2], 0.1)
+    trainer.hash = "test"
+
+    print(trainer)
+    assert (
+        capsys.readouterr().out
+        == "hash: test\nnodes: [2, 4, 2]\nlearning_rate: 0.1\n-----\n"
+    )
